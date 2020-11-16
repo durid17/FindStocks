8d978bce57c51047d4a8d82db4daa44429c43d22
demetre <durid17@freeuni.edu.ge>
2020-11-16 11:32:32 +0400

    responsive filter

--- js/main.js
+++ js/main.js
@@ -3,37 +3,36 @@ var filterMenuHTML =
 <div class="filter-menu">
 
 	<div class="filter-header-div">
-		<h2>Filter</h2>
+		<h1>Filter</h1>
 	</div>
 
 	<div class="filter-block">
-		<h5 class="filter-block-title">Type</h5>
-		<label class="radio-container">All
+		<h2 class="filter-block-title">Type</h2>
+		<label class="checkbox-container">All
 			<input type="checkbox" checked="checked" name="type">
-			<span class="checkmark"></span>
+			<span class="checkbox-checkmark"></span>
 		</label>
-		<label class="radio-container">Tech
+		<label class="checkbox-container">Tech
 			<input type="checkbox" name="type">
-			<span class="checkmark"></span>
+			<span class="checkbox-checkmark"></span>
 		</label>
-		<label class="radio-container">Farma
+		<label class="checkbox-container">Farma
 			<input type="checkbox" name="type">
-			<span class="checkmark"></span>
+			<span class="checkbox-checkmark"></span>
 		</label>
-		<label class="radio-container">Other
+		<label class="checkbox-container">Other
 			<input type="checkbox" name="type">
-			<span class="checkmark"></span>
+			<span class="checkbox-checkmark"></span>
 		</label>
 	</div>
 
 	<div class="filter-block">
-		<h5 class="filter-block-title">Prices</h5>
-		<div id="filter-price" class="filter-block-content">
-			<label for="from">from:</label>
-			<input type="number" id="from" name="from" placeholder="From">
-			<label for="to">to:</label>
-			<input type="number" id="to" name="to" placeholder="To">
-			<label for="$">$</label>
+		<h2 class="filter-block-title">Prices</h2>
+		<div class="filter-block-content">
+			<label for="from">From:</label>
+			<input type="number" class="from" name="from" placeholder="From">
+			<label for="to">To:</label>
+			<input type="number" class="to" name="to" placeholder="To">
 		</div>
 	</div>
 
@@ -50,16 +49,12 @@ var filterMenuHTML =
 	</div>
 
 	<div class="filter-block">
-		<h4>Filter by current value</h4>
+		<h3>Filter by current value</h3>
 		<button>Filter by current value</button>
 	</div>
 
-
 	<div class="filter-block">
-	</div>
-
-	<div class="filter-block">
-		<h4>Filter by increase</h4>
+		<h2>Filter by increase</h2>
 		<label class="radio-container">1D
 			<input type="radio" checked="checked" name="time-frame">
 			<span class="checkmark"></span>
@@ -95,7 +90,7 @@ var homeHTML =
     `
 
 <div id="filter-modal" class="filter-modal">
-	<img id="filter-image" src="img/filter.png" width="50px" height="50px">
+	<img id="filter-image" src="img/filter.png">
 	<div id="filter-modal-text" class="filter-modal-text">
 		<h2 id="filter-modal-text-header">Filter</h2>
 	</div>
@@ -116,7 +111,9 @@ var homeHTML =
 `
 
 var contactHTML = `<h1>Contact<\h1>`
-var aboutHTML = `<h1>About<\h1>`
+var aboutHTML = `
+<h1>About<\h1>
+`
 
 var root = null;
 var useHash = true;
@@ -139,11 +136,11 @@ router.on({
         console.log('contact');
     },
     '/about': function () {
-        toggleHamburgerMenu();
         document.getElementById('main').innerHTML = aboutHTML;
         console.log('about');
     },
     '*': function () {
+        document.getElementById('modal-content').innerHTML = filterMenuHTML;
         document.getElementById('main').innerHTML = homeHTML;
         console.log('home');
     },
